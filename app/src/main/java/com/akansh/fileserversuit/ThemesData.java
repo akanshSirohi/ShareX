bb7cfb1e4b50b8ea39d54dce54542a83b7ad39d1
Akansh Sirohi <34837239+akanshSirohi@users.noreply.github.com>
2023-02-21 12:52:16 +0530

    More themes added and few fixes

--- app/src/main/java/com/akansh/fileserversuit/ThemesData.java
+++ app/src/main/java/com/akansh/fileserversuit/ThemesData.java
@@ -2,10 +2,22 @@ package com.akansh.fileserversuit;
 
 public class ThemesData {
     private String[] displayList = {
-            "Cyborg (Default)","Cosmo","Cerulean","Journal","Litera","Lumen","Materia","Morph"
+            "Cyborg (Default)","Cosmo",
+            "Cerulean","Journal",
+            "Litera","Lumen",
+            "Pulse","Simplex",
+            "Sketchy","Spacelab",
+            "United","Vapor",
+            "Zephyr"
     };
     private String[] prefixes = {
-            "cyborg","cosmo","cerulean","journal","litera","lumen","materia","morph"
+            "cyborg","cosmo",
+            "cerulean","journal",
+            "litera","lumen",
+            "pulse","simplex",
+            "sketchy","spacelab",
+            "united","vapor",
+            "zephyr"
     };
 
     public String[] getDisplayList() {
